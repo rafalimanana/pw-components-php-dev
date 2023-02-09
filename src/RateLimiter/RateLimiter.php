6c2755387ecb2f8dfaf48be1270e733c648ed0eb
rafalimanana <rafalimananafabrice@gmail.com>
2023-02-09 15:44:48 +0300

    ratelimiter factory

--- src/RateLimiter/RateLimiter.php
+++ src/RateLimiter/RateLimiter.php
@@ -5,19 +5,36 @@ namespace Pw\RateLimiter;
 class RateLimiter
 {
     /**
+     * RateLimiterFactory
+     */
+    private $rateLimiterFactory = null;
+
+    /**
+     * Class constructor
+     *
      * @param $rateLimiterFactory
+     */
+    public function __construct($rateLimiterFactory = null)
+    {
+        if ($rateLimiterFactory) {
+            $this->rateLimiterFactory = $rateLimiterFactory;
+        }
+    }
+    /**
      * @return boolean
      */
-    public function rateLimiter($rateLimiterFactory)
+    public function isConsumeRateLimiter()
     {
-        $limiter = $rateLimiterFactory->create(
+        if (!$this->rateLimiterFactory) {
+            return false;
+        }
+        $limiter = $this->rateLimiterFactory->create(
             $_SERVER['REMOTE_ADDR']
         );
         if (false === $limiter->consume()->isAccepted()) {
             //Trop de demandes;
             return true;
         }
-
         return false;
     }
 }
